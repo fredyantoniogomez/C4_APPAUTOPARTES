0f6b3ff466083d939a846b8d26c7e9ddc60c15b5
aquí <fredy.gomez2.mt@correo.usa.edu.co>
2021-12-05 08:42:38 -0500

    RETO1
    Lanzamiento reto 1 en GIT

---
+++ reto1/app/src/main/java/usa/sesion1/reto1/MainActivity.java
@@ -0,0 +1,102 @@
+package usa.sesion1.reto1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import usa.sesion1.reto1.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Snackbar: Otra Forma del Toast", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+    public void seleccion (View view){
+        switch (view.getId()){
+            case R.id.btnp1:
+            Toast.makeText(this, "Bandeja Separarador Hilux:AGREGADO", Toast.LENGTH_SHORT).show();
+            break;
+            case R.id.btnp2:
+                Toast.makeText(this, "Carburador Standar ZCL-154:AGREGADO", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnp3:
+                Toast.makeText(this, "Empaquetadura L525 MAZDA:AGREGADO", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnp4:
+                Toast.makeText(this, "Recambio sistema electrico FORD:AGREGADO", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnp5:
+                Toast.makeText(this, "STOP TOYOTA MADEIN JAPAN:AGREGADO", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.btns1:
+                Toast.makeText(this, "Alineacion y Balanceo $50.000", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btns2:
+                Toast.makeText(this, "Pintura Especializada $1.000.000", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btns3:
+                Toast.makeText(this, "Capacitacion Mantenimiento Automotriz $600.000", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btns4:
+                Toast.makeText(this, "Despinchada en taller $20.000", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btns5:
+                Toast.makeText(this, "Despinchada Domicilio Día $30.000", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
---
+++ reto1/app/src/main/java/usa/sesion1/reto1/ui/home/HomeFragment.java
@@ -0,0 +1,52 @@
+package usa.sesion1.reto1.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import usa.sesion1.reto1.R;
+import usa.sesion1.reto1.databinding.FragmentHomeBinding;
+    
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    private FragmentHomeBinding binding;
+    View vista;
+    Button sal1;
+
+    public View onCreateView( LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
